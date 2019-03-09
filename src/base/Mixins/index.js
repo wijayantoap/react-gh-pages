2983698e7f0b274e94df99974bc37b314e049854
wijayantoap <wijayanto.a.p@gmail.com>
2019-03-09 23:06:30 +0700

    Add Mixins

---
+++ src/base/Mixins/index.js
@@ -0,0 +1,26 @@
+import { css } from 'styled-components';
+
+// Only use this as a reference
+
+const Mixins = css`
+
+    /* // phone 600px */
+    @media only screen and (max-width: 37.5em) {
+        
+    }
+
+    /* // tab-port 900px */
+    @media only screen and (max-width: 56.25em) {
+        
+    }
+
+    /* // tab-land 1200px */
+    @media only screen and (max-width: 75em) {
+        
+    }
+
+    /* // big-desktop 1200px */
+    @media only screen and (max-width: 112.5em) {
+        
+    }
+`;
