da0a2f9b8e5303cfb5cdcae1af00b5c4194c81e4
wijayantoap <wijayantoap@codex.works>
2019-03-09 18:18:04 +0700

    Updates

---
+++ precache-manifest.21d8e8f56b6b7f6777fc667586aaf899.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react-gh-pages/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "cde22863ffca018955eb",
+    "url": "/react-gh-pages/static/js/runtime~main.4c618e6a.js"
+  },
+  {
+    "revision": "c97a07bdd9c4a132ad7b",
+    "url": "/react-gh-pages/static/js/main.b9f14da4.chunk.js"
+  },
+  {
+    "revision": "a539ea9d3b1acab9c730",
+    "url": "/react-gh-pages/static/js/2.948d4d77.chunk.js"
+  },
+  {
+    "revision": "c97a07bdd9c4a132ad7b",
+    "url": "/react-gh-pages/static/css/main.8b78e45a.chunk.css"
+  },
+  {
+    "revision": "83066fffdd1d22a43f1d4daaa0a52ce4",
+    "url": "/react-gh-pages/index.html"
+  }
+];
