2a7a61930a78ec0d08794b77c946929e8906147d
wijayantoap <wijayantoap@codex.works>
2019-03-09 21:06:07 +0700

    Add base folder

---
+++ src/base/Animation/index.js
@@ -0,0 +1,45 @@
+import styled, { keyframes } from 'styled-components';
+
+export const moveInRight = keyframes`
+    0% {
+        opacity: 0;
+        transform: translateX(10rem);
+    }
+
+    80% {
+        transform: translateX(-1rem);
+    }
+
+    100% {
+        opacity: 1;
+        transform: translate(0)
+    }
+`;
+
+export const moveInLeft = keyframes`
+    0% {
+        opacity: 0;
+        transform: translateX(-10rem);
+    }
+
+    80% {
+        transform: translateX(1rem);
+    }
+
+    100% {
+        opacity: 1;
+        transform: translate(0)
+    }
+`;
+
+export const moveInBottom = keyframes`
+    0% {
+        opacity: 0;
+        transform: translateY(3rem);
+    }
+
+    100% {
+        opacity: 1;
+        transform: translate(0)
+    }
+`;
---
+++ src/base/Typography/index.js
@@ -0,0 +1,32 @@
+import styled from 'styled-components';
+import { moveInLeft, moveInRight } from '../Animation';
+
+export const HeadingPrimary = styled.h1`
+    color: #fff;
+
+    backface-visibility: hidden;
+    margin-bottom: 6rem;
+`;
+
+export const HeadingPrimary__main = styled.span`
+    display: block;
+    font-size: 6rem;
+    font-weight: 400;
+    letter-spacing: 3.5rem;
+
+    animation-name: ${moveInLeft};
+    animation-duration: 1s;
+    animation-timing-function: ease-out;
+
+    /* @include respond(phone {
+
+    }) */
+`;
+
+export const HeadingPrimary__sub = styled.span`
+    display: block;
+    font-size: 2rem;
+    font-weight: 800;
+    letter-spacing: 1rem;
+    animation: ${moveInRight} 1s ease-out
+`;
