eb852decbd5ff800b44da1e9a66144d0d3295e49
wijayantoap <wijayanto.a.p@gmail.com>
2019-03-24 01:04:11 +0700

    Testing out some libs + using random img

---
+++ src/components/HomeHeader/index.js
@@ -0,0 +1,89 @@
+import React from 'react';
+import styled from 'styled-components';
+import Grid from '@material-ui/core/Grid';
+import { Link } from "react-router-dom";
+
+const Header = styled.div`
+    display: block;
+    position: fixed;
+    padding: 0.8rem;
+    margin: 0 auto;
+    top: 0;
+    left: 0;
+    width: 100%;
+    z-index: 100;
+    background-color: ${props => props.displayHeader && 'red'};
+    -webkit-box-shadow: ${props => props.boxShadow ? props.boxShadow : 'none'};
+    -moz-box-shadow: ${props => props.boxShadow ? props.boxShadow : 'none'};
+    box-shadow: ${props => props.boxShadow ? props.boxShadow : 'none'};
+    transition: 0.2s;
+    border-bottom: 1px solid #e2e2e2;
+`;
+
+const Container = styled.div`
+    width: 110rem;
+    margin: 0 auto;
+
+    @media only screen and (max-width: 75em) {
+        width: 90rem;
+    }
+
+    @media only screen and (max-width: 56.25em) {
+        width: 60rem;
+    }
+
+    @media only screen and (max-width: 37.5em) {
+        width: 40rem;
+    }
+    
+`;
+
+class ForumHeader extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            displayHeader: 'block',
+            open: false,
+        }
+    }
+
+    componentDidMount() {
+        window.addEventListener('scroll', this.scrollFunction);
+    }
+    
+    componentWillUnmount() {
+        window.removeEventListener('scroll', this.scrollFunction);
+    }
+
+    scrollFunction = () => {
+        if (document.body.scrollTop > 310 || document.documentElement.scrollTop > 310) {
+            this.setState({
+                displayHeader: 'none'
+            })
+        } else {
+            this.setState({
+                displayHeader: 'block'
+            })
+        }
+    }
+
+    render() {
+    
+        return(
+            <div>
+                <Header className="header" boxShadow={this.state.boxShadow}>
+                    <Container>
+                        <Grid container alignItems="center" wrap="nowrap" justify="center">                    
+                            <Grid item>
+                                <Link to="/" style={{ textDecoration: 'none', color: 'white', display: `${this.state.displayHeader}` }}><span className="signature" style={{ fontSize: '4.7rem' }}>wijayantoap</span></Link>
+                            </Grid>
+                            {this.state.displayHeader === 'none' && <h1 style={{ fontFamily: 'Germania One, cursive', color: 'white', fontSize: '5rem', margin: 0 }}>Wijayanto A.P</h1>}
+                        </Grid>
+                    </Container>
+                </Header>
+            </div>
+        );
+    }
+}
+
+export default ForumHeader;
--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -3,15 +3,110 @@ import { Link } from "react-router-dom";
 
 import { Header, Header__logo_box, Header__logo, Header__text_box } from '../../layout/Header';
 import { HeadingPrimary, HeadingPrimary__main, HeadingPrimary__sub } from '../../base/Typography';
+import HomeHeader from '../../components/HomeHeader';
 
 import logo from '../../img/logo-white.png'
 import { Button } from '../../components/Button';
 
+import wallpaper1 from '../../img/junglefantasy.jpg';
+import wallpaper2 from '../../img/castlewaterfall.jpg';
+import wallpaper3 from '../../img/magiciandragon.jpg';
+import wallpaper4 from '../../img/rockstatues.jpg';
+import wallpaper5 from '../../img/shipsealight.jpg';
+
+import ProgressBar from "react-scroll-progress-bar";
+import LazyHero from 'react-lazy-hero';
+
 class Home extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            displayHeader: 'block',
+            scrollColor: 'green'
+        }
+    }
+
+    componentDidMount() {
+        window.addEventListener('scroll', this.scrollFunction);
+    }
+    
+    componentWillUnmount() {
+        window.removeEventListener('scroll', this.scrollFunction);
+    }
+
+    scrollFunction = () => {
+        if (document.body.scrollTop < 700 || document.documentElement.scrollTop < 700) {
+            this.setState({
+                scrollColor: 'green'
+            })
+        } 
+
+        if (document.body.scrollTop > 310 || document.documentElement.scrollTop > 310) {
+            this.setState({
+                displayHeader: 'none'
+            })
+        } else {
+            this.setState({
+                displayHeader: 'block'
+            })
+        }
+
+        if (document.body.scrollTop > 700 || document.documentElement.scrollTop > 700) {
+            this.setState({
+                scrollColor: 'red'
+            })
+        } 
+          
+        if (document.body.scrollTop > 1400 || document.documentElement.scrollTop > 1400) {
+            this.setState({
+                scrollColor: 'yellow'
+            })
+        }  
+
+        if (document.body.scrollTop > 2100 || document.documentElement.scrollTop > 2100) {
+            this.setState({
+                scrollColor: 'blue'
+            })
+        }  
+        
+        if (document.body.scrollTop > 2800 || document.documentElement.scrollTop > 2800) {
+            this.setState({
+                scrollColor: 'black'
+            })
+        }  
+    }
+
     render() {
         return(
-            <div className="home">
-                <Header>
+            <div>
+                <HomeHeader />
+                <ProgressBar height="6px" bgcolor={this.state.scrollColor} style={{ top: 50 }}/>
+                <div>
+                <LazyHero imageSrc={wallpaper1} opacity="0" minHeight="100vh" parallaxOffset="100">
+                    <span style={{ fontFamily: 'Germania One, cursive', color: 'white', fontSize: '5rem', display: `${this.state.displayHeader}` }}>Wijayanto A.P</span>
+                </LazyHero>
+                </div>
+                <div>
+                <LazyHero imageSrc={wallpaper2} opacity="0" minHeight="100vh" transitionTimingFunction="ease-in-out">
+                    <h1 style={{ fontFamily: 'Germania One, cursive', color: 'white', fontSize: '5rem' }}>Wijayanto A.P</h1>
+                </LazyHero>
+                </div>
+                <div>
+                <LazyHero imageSrc={wallpaper3} opacity="0" minHeight="100vh">
+                    <h1 style={{ fontFamily: 'Germania One, cursive', color: 'white', fontSize: '5rem' }}>Wijayanto A.P</h1>
+                </LazyHero>
+                </div>
+                <div>
+                <LazyHero imageSrc={wallpaper4} opacity="0" minHeight="100vh">
+                    <h1 style={{ fontFamily: 'Germania One, cursive', color: 'white', fontSize: '5rem' }}>Wijayanto A.P</h1>
+                </LazyHero>
+                </div>
+                <div>
+                <LazyHero imageSrc={wallpaper5} opacity="0" minHeight="100vh">
+                    <h1 style={{ fontFamily: 'Germania One, cursive', color: 'white', fontSize: '5rem' }}>Wijayanto A.P</h1>
+                </LazyHero>
+                </div>
+                {/* <Header>
                     <Header__logo_box>
                         <Header__logo src={logo} />
                     </Header__logo_box>
@@ -25,7 +120,7 @@ class Home extends React.Component {
                         <div><Link to={'/forum'}><Button>Forum</Button></Link></div>
                         <div><Link to={'/blog'}><Button style={{ marginTop: 16 }} color={'white'} background={'#2998ff'}>Blog</Button></Link></div>
                     </Header__text_box>
-                </Header>
+                </Header> */}
             </div>
         );
     }
