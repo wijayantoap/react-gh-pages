59b6b4ccdfc6fcf2d4f5d4126c166b65be38d04b
wijayantoap <wijayanto.a.p@gmail.com>
2019-03-28 06:08:15 +0700

    Updates

---
+++ precache-manifest.002335141712d35aa63193e3e1645a61.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = [
+  {
+    "revision": "a938900b17114b3badf1fca0c231e3e8",
+    "url": "/static/media/nightcityvector.a938900b.jpg"
+  },
+  {
+    "revision": "25de3eab13be44fbb69d",
+    "url": "/static/css/main.ecef8445.chunk.css"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.c5541365.js"
+  },
+  {
+    "revision": "f701eb7b00b50f27f602efea12ac1672",
+    "url": "/static/media/programminghero.f701eb7b.jpg"
+  },
+  {
+    "revision": "84e49ee866d54c6da14c",
+    "url": "/static/js/2.4d943d43.chunk.js"
+  },
+  {
+    "revision": "b04eddd71b88ca37c626d5650c828796",
+    "url": "/static/media/discover.b04eddd7.jpeg"
+  },
+  {
+    "revision": "cc846ebace26d266fceef34ec2ad0a75",
+    "url": "/static/media/tavern.cc846eba.jpg"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "25de3eab13be44fbb69d",
+    "url": "/static/js/main.237f83d9.chunk.js"
+  },
+  {
+    "revision": "e1449fc79a6237dab8b83b3b5b294d68",
+    "url": "/static/media/nightcity.e1449fc7.jpg"
+  },
+  {
+    "revision": "db6309f3494c81a5deff76b07de507ae",
+    "url": "/static/media/image404.db6309f3.jpg"
+  },
+  {
+    "revision": "5c733f46f77bc3e5efbb35cbfc9077b1",
+    "url": "/static/media/dubaivector.5c733f46.jpg"
+  },
+  {
+    "revision": "5ab416983daf17c8bbcb66a7dc0fe083",
+    "url": "/static/media/moderncity.5ab41698.jpg"
+  },
+  {
+    "revision": "8d4a08bdefc57f74148d1b11b0bfd2da",
+    "url": "/static/media/coffeewallpaper.8d4a08bd.jpg"
+  },
+  {
+    "revision": "84e49ee866d54c6da14c",
+    "url": "/static/css/2.b35f471c.chunk.css"
+  },
+  {
+    "revision": "7c7ab8a214583c31b4f741edcfa9233e",
+    "url": "/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.32679f487c63169b5af8ae0ad1c30e4b.js"
+  "/precache-manifest.002335141712d35aa63193e3e1645a61.js"
 );
 
 workbox.clientsClaim();
